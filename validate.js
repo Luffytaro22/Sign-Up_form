fa796d5f80afebefb1ad41b2a225afd28150db39
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-07 09:56:57 -0500

    Solved eslint errors

--- validate.js
+++ validate.js
@@ -3,11 +3,11 @@ const valPassword = document.getElementById('pss-confirm');
 
 function validate() {
   if (valPassword.value !== password.value) {
-      valPassword.setCustomValidity('Passwords do not match');
-    } else {
-      valPassword.setCustomValidity('');
-    }
+    valPassword.setCustomValidity('Passwords do not match');
+  } else {
+    valPassword.setCustomValidity('');
   }
+}
 
 valPassword.addEventListener('input', validate);
 password.addEventListener('input', validate);
