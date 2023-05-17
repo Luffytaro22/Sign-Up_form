5ad3501713efe7b694234499a6f760c5fe46795d
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-17 16:10:26 -0500

    Added the display errors functions

--- validate.js
+++ validate.js
@@ -1,11 +1,67 @@
 const password = document.getElementById('password');
 const valPassword = document.getElementById('pss-confirm');
+const firstName = document.querySelector('#first-name');
+const spanFName = document.querySelector('#first-name + span');
+const lastName = document.querySelector('#last-name');
+const spanLName = document.querySelector('#last-name + span');
+const email = document.querySelector('#email');
+const spanEmail = document.querySelector('#email + span');
+const phone = document.querySelector('#phone');
+const spanPhone = document.querySelector('#phone + span');
+const spanPass = document.querySelector('#password + span');
+const spanValid = document.querySelector('#pss-confirm + span');
 
+/* Validates the information in the Confirm Password input */
 function validate() {
-  if (valPassword.value !== password.value) {
-    valPassword.setCustomValidity('Passwords do not match');
-  } else {
-    valPassword.setCustomValidity('');
+  if (valPassword.validity.valueMissing) {
+    spanValid.textContent = '* Enter a value';
+  } else if (valPassword.value !== password.value) {
+    spanValid.textContent = '* Passwords do not match';
+  }
+}
+
+/* A function that displays the errors in the Name's inputs */
+function errorName() {
+  if (firstName.validity.valueMissing) {
+    spanFName.textContent = '* Enter a value';
+  } else if (firstName.validity.typeMismatch) {
+    spanFName.textContent = '* Please enter a valid value';
+  }
+  if (lastName.vallidity.valueMissing) {
+    spanLName.textContent = '* Enter a value';
+  } else if (lastName.validity.typeMismatch) {
+    spanLName.textContent = '* Please enter a valid value';
+  }
+}
+
+/* A function that displays the errors in the email input */
+function errorEmail() {
+  if (email.validity.valueMissing) {
+    spanEmail.textContent = '* Enter a value';
+  } else if (email.validity.typeMismatch) {
+    spanEmail.textContent = '* The values must be the form "@user.ab"';
+  }
+}
+
+/* A function that displays the errors in the phone input */
+function errorPhone() {
+  if (phone.validity.valueMissing) {
+    spanPhone.textContent = '* Enter a value';
+  } else if (phone.validity.typeMismatch) {
+    spanPhone.textContent = '* Please enter just numbers';
+  } else if (phone.validity.tooShort) {
+    spanPhone.textContent = `* Enter at least ${phone.minLength} characters, you have ${phone.value.length}.`;
+  } else if (phone.validity.tooLong) {
+    spanPhone.textContent = `* Enter a maximun of ${phone.axLength} characters.`;
+  }
+}
+
+/* A function that displays the errors in the password input */
+function errorPassword() {
+  if (password.validity.valueMissing) {
+    spanPass.textContent = '* Enter a value';
+  } else if (password.validity.typeMismatch) {
+    spanPass.textContent = '* Please enter a valid value';
   }
 }
 
